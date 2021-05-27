eaa0f0f76e402619f6ad59f829e326ebb8795d96
Niels <niels@statik.be>
2021-05-27 15:05:18 +0200

    Fix pagination

---
+++ docs/pages/Components/Pagination.md
@@ -0,0 +1,25 @@
+# Pagination
+
+## Examples
+
+### Default pagination
+
+<r-app>
+<r-pagination :model-value="123" :pages="123"/>
+</r-app>
+
+```vue
+<r-pagination v-model="page" :pages="123" />
+```
+
+<div class="api">
+
+## API
+
+### Props
+
+| Name  | Type   | Default |     |
+| ----- | ------ | ------- | --- |
+| pages | Number |         |     |
+
+</div>
---
+++ src/components/Pagination/PaginationButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    type="button"
+    class="m-1 px-1 py-0 text-button border-0 font-base font-bold leading-[40px] min-w-[40px] cursor-pointer transition-colors"
+    :class="{
+      'bg-primary text-primary-contrast': isActive,
+      'bg-gray-100 text-primary hover:bg-gray-200': !isActive,
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+};
+</script>
