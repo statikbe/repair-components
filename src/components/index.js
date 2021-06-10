13a72cead30268c715efb654a5cf4c39d024e023
Niels <niels@statik.be>
2021-06-10 08:22:59 +0200

    Remove broken modal component

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ export { RFormCheckbox, RForm, RFormField, RFormLocation, RFormInput, RFormRadio
 export { RIcon } from './Icon';
 export { RGrid, RGridItem } from './Grid';
 export { RLink } from './Link';
-export { RModal } from './Modal';
+// export { RModal } from './Modal';
 export { RPagination } from './Pagination';
 export { RPanel } from './Panel';
 export { RSection } from './Section';
